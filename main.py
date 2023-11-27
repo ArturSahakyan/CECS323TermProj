0dcc325d8651d8dd2944fa01e42ec32928f1fc49
Artur Sahakyan <45978026+ArturSahakyan@users.noreply.github.com>
2023-11-27 09:06:24 -0800

    Revert "Setting Up Develop Branch & Merged in Menu Feat"

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-# API Imports
-from pymongo import MongoClient
-import certifi
 
-# Local Imports
-from MenuDefs import menu_main
 
 if __name__ == "__main__":
-    user_action: str = ""
-    while user_action != menu_main.last_action():
-        user_action = menu_main.menu_prompt()
-        exec(user_action)
-        print("")
+    print("#CECS323ISKILLINGME")
